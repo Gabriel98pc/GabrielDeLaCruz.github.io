b33c959fe5da2d298bacb635d173d787e9fcacf0
gabrieldelacruzrolldbox <gabriel.delacruz@rolldbox.com>
2026-04-03 18:53:46 -0300

    Updates

--- js/MyProjects.js
+++ js/MyProjects.js
@@ -3,6 +3,17 @@ export const myprojects = {
     gifs: [
         {
             file: '',
+            youtubeId: 'R15fzzR7OJE',
+            title: 'Procedural Spider',
+            link: '#',
+            description: `
+                <p>
+                    Procedural spider without animations using full Custom Control Rig.
+                </p>
+            `
+        },
+        {
+            file: '',
             youtubeId: 'ctPKC_H4xCU',
             title: 'Procedural Velociraptor',
             link: '#',
