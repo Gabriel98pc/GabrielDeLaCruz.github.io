1a0ad80530b892bfff763a734114a65aa6e302bd
gabrieldelacruzrolldbox <gabriel.delacruz@rolldbox.com>
2026-05-26 13:47:08 -0300

    updates

--- js/MyBlenderWork.js
+++ js/MyBlenderWork.js
@@ -6,6 +6,12 @@ export const myblenderwork = {
             title: 'Blender - Stylized Shark Rigg & Animation',            
             link: 'https://www.youtube.com/watch?v=M_m3-0P70bs',
             description: 'I rigged and animated this 3D model of a stylized shark; the model itself wasnt made by me, only the rig and animation.'
+        },
+        {
+            file: 'Blender_DocOcTest.gif',
+            title: 'Blender - Doctor Octopus Arms Rigg',            
+            link: '',
+            description: 'I made the rigging for these arms, inspired by the metal arms of Doc Ock from Spider-Man.'
         }
     ]
 };
